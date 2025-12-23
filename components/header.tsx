cc7cf81992111f47de67ef2430e53cef20c3fb26
Nebu08 <6onzacris15@gmail.com>
2025-12-22 21:41:57 -0500

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -64,7 +64,7 @@ export function Header() {
           onClick={() => scrollToSection("quotes")}
           className="btn-primary text-sm"
         >
-          ¡Cotiza!
+          Solicitar Cotización
         </button>
       </div>
     </header>
